b3617c2518c227f952ed1030650d696c6184f538
Nattaphot Vittangkrun <nattaphotvittangkrun@gmail.com>
2025-07-02 04:17:03 +0700

    CHORE fixing cors

--- frontend/pitwall/next.config.ts
+++ frontend/pitwall/next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/:path*`,
-      },
-    ];
-  },
+  
 };
 
 export default nextConfig;
